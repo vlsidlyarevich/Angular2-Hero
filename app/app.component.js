e5540f2634648e54c621e2143a09e822586d9695
vlsidlyarevich <vladislav.sidlyarevich@azati.com>
2016-05-27 09:25:18 +0000

    master:list of heroes added

--- app/app.component.js
+++ app/app.component.js
@@ -22,15 +22,28 @@ var AppComponent = (function () {
             id: 1,
             name: 'windstorm'
         };
+        this.heroes = HEROES;
     }
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: "\n    <h1>{{title}}</h1>\n    <h2>{{hero.name}} details!</h2> \n    <div><label>id: </label>{{hero.id}}</div>\n    <div>\n    <label>name: </label>\n    <input [(ngModel)]=\"hero.name\" placeholder=\"name\">\n    </div>"
+            template: "\n    <h1>{{title}}</h1>\n    <h2>My Heroes</h2>\n    <ul class=\"heroes\">\n        <li *ngFor=\"let hero of heroes\">\n            <span class=\"badge\">{{hero.id}}</span> {{hero.name}}\n        </li>\n    </ul>\n    "
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
     return AppComponent;
 }());
 exports.AppComponent = AppComponent;
+var HEROES = [
+    { "id": 11, "name": "Mr. Nice" },
+    { "id": 12, "name": "Narco" },
+    { "id": 13, "name": "Bombasto" },
+    { "id": 14, "name": "Celeritas" },
+    { "id": 15, "name": "Magneta" },
+    { "id": 16, "name": "RubberMan" },
+    { "id": 17, "name": "Dynama" },
+    { "id": 18, "name": "Dr IQ" },
+    { "id": 19, "name": "Magma" },
+    { "id": 20, "name": "Tornado" }
+];
 //# sourceMappingURL=app.component.js.map
--- app/app.component.ts
+++ app/app.component.ts
@@ -7,12 +7,13 @@ export class Hero {
     selector: 'my-app',
     template: `
     <h1>{{title}}</h1>
-    <h2>{{hero.name}} details!</h2> 
-    <div><label>id: </label>{{hero.id}}</div>
-    <div>
-    <label>name: </label>
-    <input [(ngModel)]="hero.name" placeholder="name">
-    </div>`
+    <h2>My Heroes</h2>
+    <ul class="heroes">
+        <li *ngFor="let hero of heroes">
+            <span class="badge">{{hero.id}}</span> {{hero.name}}
+        </li>
+    </ul>
+    `
 })
 export class AppComponent {
 
@@ -21,6 +22,17 @@ export class AppComponent {
         id: 1,
         name: 'windstorm'
     };
-
-
+    public heroes = HEROES;
 }
+var HEROES:Hero[] = [
+    {"id": 11, "name": "Mr. Nice"},
+    {"id": 12, "name": "Narco"},
+    {"id": 13, "name": "Bombasto"},
+    {"id": 14, "name": "Celeritas"},
+    {"id": 15, "name": "Magneta"},
+    {"id": 16, "name": "RubberMan"},
+    {"id": 17, "name": "Dynama"},
+    {"id": 18, "name": "Dr IQ"},
+    {"id": 19, "name": "Magma"},
+    {"id": 20, "name": "Tornado"}
+];
