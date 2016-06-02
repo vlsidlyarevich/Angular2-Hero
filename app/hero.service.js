1584405593a00b5ff48d7c9ff03fbf8b4cb2c4d4
vlsidlyarevich <vlsidlyarevich@gmail.com>
2016-06-02 23:10:17 +0300

    master get hero by id added

--- app/hero.service.js
+++ app/hero.service.js
@@ -16,6 +16,10 @@ var HeroService = (function () {
     HeroService.prototype.getHeroes = function () {
         return Promise.resolve(mock_heroes_1.HEROES);
     };
+    HeroService.prototype.getHero = function (id) {
+        return Promise.resolve(mock_heroes_1.HEROES)
+            .then(function (heroes) { return heroes.filter(function (hero) { return hero.id == id; })[0]; });
+    };
     HeroService = __decorate([
         core_1.Injectable(), 
         __metadata('design:paramtypes', [])
--- app/hero.service.ts
+++ app/hero.service.ts
@@ -6,4 +6,9 @@ export class HeroService{
     getHeroes(){
         return Promise.resolve(HEROES);
     }
+
+    getHero(id:number){
+        return Promise.resolve(HEROES)
+            .then(heroes => heroes.filter(hero => hero.id == id)[0] );
+    }
 }
